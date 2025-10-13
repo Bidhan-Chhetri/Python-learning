9e82f1b3edd47cebb8e48d7f3e416f3880f0013d
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-10-13 21:08:39 +0545

    deep folder

---
+++ problem-set-1/deep/deep.py
@@ -0,0 +1,11 @@
+def main():
+    question = str(input("What is the Answer to the Question of Life, the Universe, and Everything? "))
+    check(question)
+
+def check(value):
+    if value == "42" or value == 42 or value == "forty-two" or value == "forty two":
+        print("yes")
+    else:
+        print("no")
+
+main()
