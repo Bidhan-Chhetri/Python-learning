035f8cc340e42fd5e30a2883d414a74e8f29c089
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-10-14 10:24:46 +0545

    javascript folder

---
+++ Javascript/script.js
@@ -0,0 +1,64 @@
+// PRACTICE QUESTION 1
+
+// let input = prompt("Enter the number");
+// if (input % 5 === 0) {
+//     console.log("Number is the multiple of 5");
+// }
+// else {
+// console.log("Number is not the multiple of 5");
+// }
+
+
+// PRACTIVE QUESTION 2
+
+// let marks = prompt("Enter the marks: ");
+// if (marks >= 80 && marks <=100) {
+//     console.log("Grade A");
+// }
+
+// else if (marks >= 70 && marks <= 79) {
+//     console.log("Grade B");
+// }
+
+// else if (marks >= 60 && marks <= 69) {
+//     console.log("Grade C");
+// }
+
+// else if (marks >= 50 && marks <= 59) {
+//     console.log("Grade D");
+// }
+
+// else if (marks >= 40 && marks <= 49) {
+//     console.log("Grade E");
+// }
+
+// else {
+//     console.log("Grade F");
+// }
+
+// PRACTICE QUESTION 3
+
+// for (let i = 2; i <= 100; i+=2) {
+//     console.log("i = ",i);
+// }
+
+// PRACTIVE QUESTION 4
+
+// let value = "25";
+// let guess = prompt("Guess the number?");
+// do {
+//     if(value === guess) 
+//     {
+//         console.log("Correct guess");
+//     }
+//     else {
+//         console.log("Incorrect guess, try again");
+//     }
+// }
+// while(false);
+
+ // PRACTICE QUESTION 5
+
+ let name = prompt("Enter your Full Name");
+    string = "@" + name + name.length;
+ console.log(string);
