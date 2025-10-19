ed0b748b6813b1cede4587d21b0b5d762081741b
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-10-19 15:33:39 +0545

    twttr folder

---
+++ problem-set-2/twttr/twttr.py
@@ -0,0 +1,11 @@
+def main():
+    word = str(input("Input: "))
+    check(word)
+
+def check(word):
+    lower = word.lower()
+    for i in lower:
+        if i not in ('a', 'e', 'i', '0', 'u'):
+            print(i, end = "")
+
+main()
