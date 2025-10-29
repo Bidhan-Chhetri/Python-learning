f18f4b7fbf1455bb63c0b6def6eb4b76904f66f8
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-10-29 18:16:32 +0545

    Clean up figlet.py by removing unused code
    Removed commented-out code for input handling and font selection.

--- problem-set-4/figlet/figlet.py
+++ problem-set-4/figlet/figlet.py
@@ -18,19 +18,3 @@ else:
     figlet.setFont(font=font)
     print(figlet.renderText(get_str))
 
-# import sys
-# from pyfiglet import Figlet
-
-# get_str = input("Input: ")
-# figlet = Figlet()
-
-# if len(sys.argv) == 3:
-#     if sys.argv[1] in ["-f", "--font"]:
-#         font = sys.argv[2]
-#         if font in figlet.getFonts():
-#             figlet.setFont(font=font)
-#             print(figlet.renderText(get_str))
-#         else:
-#             sys.exit("Invalid font name.")
-# else:
-#     print(figlet.renderText(get_str))  # Use default font
