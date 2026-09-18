19e2462ebcdd4dbec95a1ca99ea2e12edb10fcd1
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2026-09-18 20:07:54 +0545

    Completed day 1

---
+++ 30-Days-Of-Python/Day1/helloWorld.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+
+print(2 + 3)    # Addition (+)
+print(2 * 2)    # Subtraction (-)
+print(3 / 2)    # Multiplication (*)
+print(3 ** 2)   # Exponential(**)
+print(3 % 2)    # Modulus (%)
+print(3 // 2)   # Floor division operator (//)
+print(10 + 4j - 3 - 2j + 8j)    # Complex number arthemetic operation    
+
+print(type(10))     # Int
+print(type(3.14))   # Float
+print(type(1 + 3j)) # Complex
+print(type('Bidhan'))   # String
+print(type([1, "Bidhan", 1.5])) # List
+print(type({'Name': 'Bidhan', 'Age': '18'}))  # Dictionary
+print(type((9.8, 3.4, 34.23)))  # Tuple
+print(type({1.5, 'Bidhan', 10}))    # Set
