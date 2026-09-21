09b326e793d0b15d4222c6844c6c473249f0766e
Bidhan-Chhetri <bidhanchhetri52@gmail.com>
2026-09-21 22:55:37 +0545

    completed some exercise of 30 days of python

---
+++ 30-Days-Of-Python/Day3/Exercises.py
@@ -0,0 +1,28 @@
+# Declare your age as int variable
+
+age = 18
+
+# Declare your height as a float number
+
+height = 5.5
+
+# Declare a variable that store complex number
+
+complexNumber = 4 + 2j + 3j -5j + 3J -1
+
+# Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h)
+
+base = float(input("Enter base : "))
+height = float(input("Enter height : "))
+
+print(f"Area = {0.5 * base * height}")
+
+# Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c)
+
+a = float(input("Enter a : "))
+b = float(input("Enter b : "))
+c = float(input("Enter c : "))
+
+print(f"The perimeter of the triangle {a + b + c}")
+
+# Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
